c73fb45e0befef03cec3d374ff72bb7cd66bb5da
Abhishek <abhishekY495.sec@gmail.com>
2023-12-08 20:02:35 +0530

    Styling

--- frontend/src/components/Dashboard/EnrolledCourseDetails.jsx
+++ frontend/src/components/Dashboard/EnrolledCourseDetails.jsx
@@ -22,16 +22,24 @@ export const EnrolledCourseDetails = ({ course }) => {
     <div className="flex flex-col mx-5">
       <Link
         to={`/courses/${course.courseId}`}
-        className="flex gap-2 border-b pb-2 mb-1"
+        className="flex flex-col border-b mb-1"
       >
-        <img
-          src={course?.thumbnail}
-          alt={course?.title}
-          className="rounded w-[80px] h-[80px] object-cover"
-        />
-        <h2 className="text-3xl font-bold py-1">{course?.title}</h2>
+        <div className="flex gap-2">
+          <img
+            src={course?.thumbnail}
+            alt={course?.title}
+            className="rounded w-[80px] h-[80px] object-cover"
+          />
+          <h2 className="text-3xl font-bold py-1">{course?.title}</h2>
+        </div>
+        <button
+          className="bg-orange-500 my-2 py-1 rounded text-xl font-bold text-white hover:bg-orange-600 transition-all"
+          onClick={unenrollCourseBtnHandler}
+        >
+          UN-ENROLL
+        </button>
       </Link>
-      <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-1 border-b pb-3">
         <p className="font-bold text-2xl">{percent}% Completed</p>
         <div className="w-[100%] border rounded-full">
           <div
@@ -40,12 +48,6 @@ export const EnrolledCourseDetails = ({ course }) => {
           ></div>
         </div>
       </div>
-      <button
-        className="bg-orange-500 my-2 py-1 rounded text-xl font-bold text-white hover:bg-orange-600 transition-all"
-        onClick={unenrollCourseBtnHandler}
-      >
-        UN-ENROLL
-      </button>
       <Syllabus syllabus={course?.syllabus} enrolled />
     </div>
   );
